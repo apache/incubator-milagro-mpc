043ca02c4837e08e70f71124351a6477fa17087d
samuele-andreoli <samuele.andreoli@yahoo.it>
2019-12-03 13:07:47 +0000

    clean paillier

--- model/sec256k1/paillier.py
+++ model/sec256k1/paillier.py
@@ -19,15 +19,18 @@ def encrypt(n, g, pt, r=None):
 
     if r is None:
         r = big.rand(n)
+
+    if DEBUG:
         print("r {}".format(hex(r)))
+
     rn = pow(r, n, n2)
     gpt = pow(g, pt, n2)
-    ct = (rn * gpt) % n2    
+    ct = (rn * gpt) % n2
 
     return ct, r
 
 def decrypt(n, l, m, ct):
-    n2 = n*n 
+    n2 = n*n
     ctl = pow(ct, l, n2) - 1
     ctln = ctl // n
     pt = big.modmul(ctln, m, n)
@@ -35,10 +38,10 @@ def decrypt(n, l, m, ct):
     if DEBUG:
         print("decrypt n2 {}\n".format(hex(n2)))
         print("decrypt ctl {}\n".format(hex(ctl)))
-        print("decrypt n {}\n".format(hex(n)))        
+        print("decrypt n {}\n".format(hex(n)))
         print("decrypt ctln {}\n".format(hex(ctln)))
         print("decrypt pt {}\n".format(hex(pt)))
-        
+
     return pt
 
 def add(a,b,n):
@@ -49,22 +52,20 @@ def add(a,b,n):
     if DEBUG:
         print("add ct1: {}\n".format(hex(a)))
         print("add ct2: {}\n".format(hex(b)))
-        print("add n: {}\n".format(hex(n)))                 
+        print("add n: {}\n".format(hex(n)))
         print("add n^2: {}\n".format(hex(n2)))
         print("add ct1 * ct2: {}\n".format(hex(c)))
-        print("add ct1 * ct2 mod n^2: {}\n".format(hex(d)))                
-    
+        print("add ct1 * ct2 mod n^2: {}\n".format(hex(d)))
+
     return d
-    
+
 def mult(a,b,n):
     n2 = n * n
-    c = pow(a, b, n2)    
+    c = pow(a, b, n2)
 
     if DEBUG:
-        print("add n: {}\n".format(hex(n)))                 
+        print("add n: {}\n".format(hex(n)))
         print("add n^2: {}\n".format(hex(n2)))
         print("add a^b mod n^2: {}\n".format(hex(c)))
-    
+
     return c
-
-
