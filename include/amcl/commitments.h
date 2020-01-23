997fdd646c76f294f2424fc21de40800924c047a
Samuele Andreoli <samuele.andreoli@yahoo.it>
2020-01-23 15:08:10 +0000

    add BC setup source and headers

--- include/amcl/commitments.h
+++ include/amcl/commitments.h
@@ -23,10 +23,11 @@ under the License.
  *
  */
 
-#ifndef COMMITMENTS
-#define COMMITMENTS
+#ifndef COMMITMENTS_H
+#define COMMITMENTS_H
 
 #include "amcl/amcl.h"
+#include "amcl/ff_2048.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -36,7 +37,7 @@ extern "C"
 /* NM Commitment Scheme API */
 
 /*! \brief Generate a commitment for the value X
- * 
+ *
  * @param RNG   CSPRNG to use for commitment
  * @param X     Value to commit to
  * @param R     Decommitment value. If RNG is null then this value is read and must be 256 bit long
@@ -45,13 +46,57 @@ extern "C"
 extern void COMMITMENTS_NM_commit(csprng *RNG, octet *X, octet *R, octet *C);
 
 /*! \brief Decommit the value X
- * 
+ *
  * @param X     Committed value
  * @param R     Decommitment value. Must be 256 bit long
  * @param C     Commitment value
  * @return      Returns 1 for a valid decommitment, 0 otherwise
  */
 extern int COMMITMENTS_NM_decommit(octet* X, octet* R, octet* C);
+
+/* Bit Commitment Setup API */
+
+#define FS_2048 MODBYTES_1024_58 * FFLEN_2048  /**< 2048 field size in bytes */
+#define HFS_2048 MODBYTES_1024_58 * HFLEN_2048 /**< Half 2048 field size in bytes */
+
+/*!
+ * \brief RSA modulus for Bit Commitment
+ */
+typedef struct
+{
+    BIG_1024_58 P[HFLEN_2048];      /**< Safe prime P = 2p+1 */
+    BIG_1024_58 Q[HFLEN_2048];      /**< Safe prime Q = 2q+1 */
+    BIG_1024_58 pq[FFLEN_2048];     /**< Precomputed product of p and q */
+    BIG_1024_58 N[FFLEN_2048];      /**< Public part of the modulus */
+    BIG_1024_58 alpha[FFLEN_2048];  /**< Secret exponent of the DLOG b1 = b0^alpha*/
+    BIG_1024_58 ialpha[FFLEN_2048]; /**< Inverse of alpha mod pq. Secret exponent of the DLOG b0 = b1^ialpha */
+    BIG_1024_58 b0[FFLEN_2048];     /**< Generator of G_pq as subgroup of Z/PQZ */
+    BIG_1024_58 b1[FFLEN_2048];     /**< Generator of G_pq as subgroup of Z/PQZ */
+} COMMITMENTS_BC_priv_modulus;
+
+/*! \brief Set up an RSA modulus and the necessary values.
+ *
+ * RNG is only used to generate the values not explicitely specified.
+ * This allows using safe primes P and Q generated externally while
+ * still randomly generating B0 and ALPHA. In turn, this allows the
+ * user to generate P and Q with ad hoc libraries for the generation of
+ * primes instead of the (slow) safe prime generation utility included
+ * here.
+ *
+ * @param RNG   CSPRNG to generate P, Q, B0 and ALPHA
+ * @param m     Private modulus to populate
+ * @param P     Safe prime 2p+1. Generated if NULL
+ * @param Q     Safe prime 2q+1. Generated if NULL
+ * @param B0    Generator of G_pq as subgroup of Z/PQZ. Generated if NULL
+ * @param ALPHA DLOG exponent for B1 = B0^ALPHA. Generated if NULL
+ */
+extern void COMMITMENTS_BC_setup(csprng *RNG, COMMITMENTS_BC_priv_modulus *m, octet *P, octet *Q, octet *B0, octet *ALPHA);
+
+/*! \brief Clean secret values from the modulus
+ *
+ * @param m     The modulus to clean
+ */
+extern void COMMITMENTS_BC_kill_priv_modulus(COMMITMENTS_BC_priv_modulus *m);
 
 #ifdef __cplusplus
 }
--- src/commitments.c
+++ src/commitments.c
@@ -48,13 +48,13 @@ void hash(octet *X, octet *R, octet *C)
 
 // Compute a commitment for the value X
 void COMMITMENTS_NM_commit(csprng *RNG, octet *X, octet *R, octet *C)
-{    
+{
     if (RNG != NULL)
     {
         OCT_rand(R, RNG, SHA256);
     }
-    
-    hash(X, R, C);    
+
+    hash(X, R, C);
 }
 
 // Verify the commitment for the value X
@@ -74,4 +74,210 @@ int COMMITMENTS_NM_decommit(octet *X, octet *R, octet *C)
     hash(X, R, &D);
 
     return OCT_comp(C, &D);
+}
+
+/* Bit Commitment Setup Definitions */
+
+/*
+ * Check if a number is a safe prime
+ */
+int is_safe_prime(BIG_1024_58 *p, BIG_1024_58 *P, csprng *RNG, int n)
+{
+#ifndef C99
+    BIG_1024_58 Pm1[FFLEN_2048], f[FFLEN_2048];
+#else
+    BIG_1024_58 Pm1[n], f[n];
+#endif
+
+    // Sieve small primes from P, p is already checked in Miller-Rabin
+    sign32 sf=4849845;/* 3*5*.. *19 */
+
+    if(FF_2048_cfactor(P, sf, n))
+    {
+        return 0;
+    }
+
+    // Check primality of p
+    if (FF_2048_prime(p, RNG, n) == 0)
+    {
+        return 0;
+    }
+
+    // Simplified primality check for safe primes using
+    // Pocklington's criterion
+    //
+    // If p is prime, P = 2p+1, 2^(P-1) = 1 mod P, then P is prime
+    FF_2048_init(f, 2, n);
+    FF_2048_copy(Pm1, P, n);
+    FF_2048_dec(Pm1, 1, n);
+
+    FF_2048_pow(f, f, Pm1, P, n);
+    FF_2048_dec(f, 1, n);
+    if (FF_2048_iszilch(f, n))
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+ * Generate a safe prime P, such that P = 2 * p + 1
+ * n is the size of P in BIGs
+ */
+void generate_safe_prime(csprng *RNG, BIG_1024_58 *p, BIG_1024_58 *P, int n)
+{
+    int lastbits;
+
+    FF_2048_random(p, RNG, n);
+    FF_2048_shr(p, n);
+
+    // Make sure p = 3 mod 4
+    lastbits = FF_2048_lastbits(p, 2);
+    FF_2048_inc(p, 3 - lastbits, n);
+
+    // P = 2p + 1
+    FF_2048_copy(P, p, n);
+    FF_2048_shl(P, n);
+    FF_2048_inc(P, 1, n);
+
+    while (!is_safe_prime(p, P, RNG, n))
+    {
+        // Increase p by 4 to keep it = 3 mod 4, P grows as 2*p
+        FF_2048_inc(p, 4, n);
+        FF_2048_inc(P, 8, n);
+    }
+}
+
+/*
+ * Find random element of order p in Z/PZ
+ * Assuming P = 2p + 1 is a safe prime, i.e. phi(P) = 2p
+ */
+void bc_generator(csprng *RNG, BIG_1024_58* x, BIG_1024_58 *p, BIG_1024_58 *P, int n)
+{
+#ifndef C99
+    BIG_1024_58 e[FFLEN_2048];
+#else
+    BIG_1024_58 e[n];
+#endif
+
+    FF_2048_randomnum(x, P, RNG, n);
+
+    // While ord(x) = 2, try the next
+    FF_2048_power(e, x, 2, P, n);
+    FF_2048_dec(e, 1, n);
+    while (FF_2048_iszilch(e, n))
+    {
+        FF_2048_inc(x, 1, n);
+
+        FF_2048_power(e, x, 2, P, n);
+        FF_2048_dec(e, 1, n);
+    }
+
+    // If ord(x) = 2p, square it.
+    FF_2048_pow(e, x, p, P, n);
+    FF_2048_dec(e, 1, n);
+    if (!FF_2048_iszilch(e, n))
+    {
+        FF_2048_power(x, x, 2, P, n);
+        FF_2048_mod(x, P, n);
+    }
+}
+
+void COMMITMENTS_BC_setup(csprng *RNG, COMMITMENTS_BC_priv_modulus *m, octet *P, octet *Q, octet *B0, octet *ALPHA)
+{
+    BIG_1024_58 p[HFLEN_2048];
+    BIG_1024_58 q[HFLEN_2048];
+    BIG_1024_58 gp[HFLEN_2048];
+    BIG_1024_58 gq[HFLEN_2048];
+    BIG_1024_58 ap[HFLEN_2048];
+    BIG_1024_58 aq[HFLEN_2048];
+
+    /* Load or generate safe primes P, Q */
+
+    if (P == NULL)
+    {
+        generate_safe_prime(RNG, p, m->P, HFLEN_2048);
+    }
+    else
+    {
+        FF_2048_fromOctet(m->P, P, HFLEN_2048);
+        FF_2048_copy(p, m->P, HFLEN_2048);
+
+        // Since P is odd, P>>1 == (P-1) / 2
+        FF_2048_shr(p, HFLEN_2048);
+    }
+
+    if (Q == NULL)
+    {
+        generate_safe_prime(RNG, q, m->Q, HFLEN_2048);
+    }
+    else
+    {
+        FF_2048_fromOctet(m->Q, Q, HFLEN_2048);
+        FF_2048_copy(q, m->Q, HFLEN_2048);
+
+        // Since Q is odd, Q>>1 == (Q-1) / 2
+        FF_2048_shr(q, HFLEN_2048);
+    }
+
+    FF_2048_mul(m->N, m->P, m->Q, HFLEN_2048);
+    FF_2048_mul(m->pq, p, q, HFLEN_2048);
+
+    /* Load or generate generator b0 and DLOG exponent alpha */
+
+    if (B0 == NULL)
+    {
+        // Find a generator of G_pq in Z/NZ using the crt to
+        // combine generators of G_p in Z/PZ and G_q in Z/QZ
+        bc_generator(RNG, gp, p, m->P, HFLEN_2048);
+        bc_generator(RNG, gq, q, m->Q, HFLEN_2048);
+
+        FF_2048_crt(m->b0, gp, gq, m->P, m->Q, HFLEN_2048);
+    }
+    else
+    {
+        FF_2048_fromOctet(m->b0, B0, FFLEN_2048);
+
+        FF_2048_dmod(gp, m->b0, m->P, HFLEN_2048);
+        FF_2048_dmod(gq, m->b0, m->Q, HFLEN_2048);
+    }
+
+    if (ALPHA == NULL)
+    {
+        FF_2048_randomnum(m->alpha, m->pq, RNG, FFLEN_2048);
+
+        // Look for invertible alpha and precompute inverse
+        FF_2048_invmodp(m->ialpha, m->alpha, m->pq, FFLEN_2048);
+        while (FF_2048_iszilch(m->ialpha, FFLEN_2048))
+        {
+            FF_2048_inc(m->alpha, 1, FFLEN_2048);
+            FF_2048_invmodp(m->ialpha, m->alpha, m->pq, FFLEN_2048);
+        }
+    }
+    else
+    {
+        // Load alpha and precompute inverse
+        FF_2048_fromOctet(m->alpha, ALPHA, FFLEN_2048);
+        FF_2048_invmodp(m->ialpha, m->alpha, m->pq, FFLEN_2048);
+    }
+
+    /* Compute b1 as b0 to the alpha using CRT */
+
+    FF_2048_dmod(ap, m->alpha, p, HFLEN_2048);
+    FF_2048_dmod(aq, m->alpha, q, HFLEN_2048);
+
+    FF_2048_skpow(gp, gp, ap, m->P, HFLEN_2048, HFLEN_2048);
+    FF_2048_skpow(gq, gq, aq, m->Q, HFLEN_2048, HFLEN_2048);
+
+    FF_2048_crt(m->b1, gp, gq, m->P, m->Q, HFLEN_2048);
+}
+
+void COMMITMENTS_BC_kill_priv_modulus(COMMITMENTS_BC_priv_modulus *m)
+{
+    FF_2048_zero(m->P, HFLEN_2048);
+    FF_2048_zero(m->Q, HFLEN_2048);
+    FF_2048_zero(m->pq, FFLEN_2048);
+    FF_2048_zero(m->alpha, FFLEN_2048);
+    FF_2048_zero(m->ialpha, FFLEN_2048);
 }
